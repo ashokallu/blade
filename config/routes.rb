2a5170e8e4f64c291993f35a98edd1fa3b5947f4
Ashok Allu <ashok.allu94@gmail.com>
2020-12-29 16:32:49 +0530

    * Routes, yarn packages upgrades and config/development.rb changes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'customers#index'
+
+  post 'customers', to: "customers#create"
+  # post 'customers/index', defaults: { format: "json" }
+
+  resources :customers do
+    collection do
+      get 'redirect_action'
+    end
+    resources :orders
+  end
 end
