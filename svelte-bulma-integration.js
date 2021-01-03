5ec0a2ae371fdae22f8ebf748a288c98f958dec3
Ashok Allu <ashok.allu94@gmail.com>
2021-01-03 13:35:47 +0530

    * upgraded webpacker from v5 to v6. * upgraded bulma version to 0.9.0.

--- svelte-bulma-integration.js
+++ svelte-bulma-integration.js
@@ -1,5 +1,8 @@
 // To process 'scss' or 'sass' files that are imported into ESM modules, 'node-sass' is a dependency npm package for webpack loaders like 'sass-loader', which means, 'sass-loader' cannot process 'scss' or 'sass' files that are imported into ESM modules, it depends on 'node-sass' npm package to be added to package.json of the webpack project.
 // In a Rails project, the webpacker gem installs some npm packages by default for configuring webpack for Rails project, 'node-sass' is installed by webpacker gem by default, and 'sass-loader' is also installed by webpacker gem by default, which means, out of the box, Rails project supports processing of .scss and .sass files that are imported into .js modules which will get processed by webpack.
 
+$ yarn add bulma
+$ yarn remove bulma // To remove bulma added as either direct dependency package or development dependency package.
+
 $ yarn add svelte-preprocess
 
